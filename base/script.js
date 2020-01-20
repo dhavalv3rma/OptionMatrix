233de8760b45f8853828413661676d53f32e3592
dhavalv3rma <dhavalverma@live.in>
2020-01-20 17:09:27 +0530

    Finished

---
+++ base/script.js
@@ -0,0 +1,62 @@
+        /*navbar on scroll*/
+        var navbar = $('nav');
+
+        window.onscroll = function () {
+
+            // pageYOffset or scrollY
+            if (window.pageYOffset > 100) {
+                $('nav').addClass('scrolled')
+
+            } else {
+                $('nav').removeClass('scrolled')
+            }
+        };
+
+        /*Homepage country accordion*/
+        $('#my-accordion').gridAccordion({
+            width: 600,
+            height: 500,
+            responsive: 'true',
+            responsiveMode: 'auto',
+            columns: 3,
+            rows: 3,
+            mouseWheel: false,
+            openPanelDuration: 400,
+            closePanelDuration: 300,
+            mouseDelay: 0,
+            autoplay: false
+        });
+
+        /*number-section*/
+        window.onscroll = function () {
+
+            if (window.pageYOffset > 1000) {
+                $('.counter').each(function () {
+                    var $this = $(this),
+                        countTo = $this.attr('data-count');
+
+                    $({
+                        countNum: $this.text()
+                    }).animate({
+                        countNum: countTo
+                    }, {
+                        duration: 9000,
+                        easing: 'linear',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            $this.text(this.countNum);
+                            //alert('finished');
+                        }
+                    });
+                });
+
+            } else {
+
+            }};
+
+
+
+        /*footer-date*/
+        $('#date').text(new Date().getFullYear());
