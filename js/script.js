8866e51972a923b57eb607500bded19f9f876407
Dhawal Verma <dhawalverma@optionm.com>
2020-01-28 18:19:25 +0530

    favicon added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+/*light/dark favicon*/
+$(document).ready(function () {
+    if (!window.matchMedia)
+        return;
+
+    var current = $('head > link[rel="icon"][media]');
+    $.each(current, function (i, icon) {
+        var match = window.matchMedia(icon.media);
+
+        function swap() {
+            if (match.matches) {
+                current.remove();
+                current = $(icon).appendTo('head');
+            }
+        }
+        match.addListener(swap);
+        swap();
+    });
+});
+
 /*Navbar on scroll*/
 window.onscroll = function () {
     var navbar = $('nav');
