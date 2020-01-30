082955abee04d17cc63f50cadc3356025d9c3ec4
Dhawal Verma <dhawalverma@optionm.com>
2020-01-30 16:05:22 +0530

    Finalized

--- js/script.js
+++ js/script.js
@@ -62,17 +62,17 @@ $('.ct-sidenav').find('button.close').on('click', function () {
 });
 
 /*on mobile click*/
-var acc = document.getElementsByClassName("accordion-mini");
+var acc = $(".accordion-mini")
 var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
         } else {
-            panel.style.display = "block";
+            panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
 }
